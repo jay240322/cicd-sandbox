718cd54d78ce59e07545e0bd34cbdba68c037c7e
jay patel <patel.jay.brij03@gmail.com>
2026-06-24 09:29:27 +0530

    integrate vite reverse proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0', // Forces Vite to listen to external network traffic in Kubernetes
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Forwards all /api requests straight to your backend container
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
